d285b47e4601d26e46bfcc2f0af5d5822d189ea2
aerask <aerask@umich.edu>
2017-07-25 14:28:53 -0400

    Start the LBQ

---
+++ lbq/__init__.py
@@ -0,0 +1,8 @@
+"""
+This is the base file of Life's Biggest Questions.
+"""
+
+from . import math
+from .math import add #from the math in this folder, I want to import add
+from .math import mult #from the math in this folder, I want to import mult
+
---
+++ lbq/math.py
@@ -0,0 +1,19 @@
+"""
+A small set of functions for doing math operations. 
+"""
+
+#Write a function named add that adds two values
+
+def add(a, b):
+    """
+    Addition between two arguments
+    """
+    return a + b
+
+def mult(a, b):
+    """
+    Multiplication between two arguments
+    """
+    return a * b
+
+
